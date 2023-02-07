a3139fc07056e53c6e080f4a33320f1a7ac89bee
김동하 <petme@gimdonghaui-MacBookPro.local>
2023-02-07 14:15:30 +0900

    #3.1 Constants

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class TikTokApp extends StatelessWidget {
   const TikTokApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
